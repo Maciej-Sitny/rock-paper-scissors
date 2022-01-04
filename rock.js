fa420fb2d0eccca02bc86524cd7cc07658b16ab7
Maciej <maciejsitny@gmail.com>
2022-01-04 23:43:37 +0100

    Add index.html and rock.js and rps.css and img/paper.png and img/rock.png and img/scissors.png

--- rock.js
+++ rock.js
@@ -1,49 +1,75 @@
 function computerPlay() {
     let decision = Math.floor(Math.random() * 3);
     if (decision ==0) {
-        return "Rock";
+        img.src="/img/rock.png";
+        return "rock";      
     }
     else if (decision == 1) {
-        return "Paper";
+        img.src="/img/paper.png"
+        return "paper";
     }
     else if (decision == 2) {
-        return "Scissors";
+        img.src="/img/scissors.png"
+        return "scissors";
     }
 }
  
- 
- 
-function game() {
-    let playerCounter =0;
-    let computerCounter =0;
-    for (let i = 0; i <5;i++){
-        let playerLower = prompt("Rock, paper or scissors?").toLowerCase();
-        let computerLower = computerPlay().toLowerCase();
-        if (playerLower == computerLower) {
-            playerCounter++;
-            computerCounter++;
-            alert(`It's a draw! Current score: you: ${playerCounter}; computer: ${computerCounter}`);
-           
-        }
-        else if ((playerLower=="rock" && computerLower=="paper") || (playerLower=="scissors" && computerLower=="rock") || (playerLower=="paper" && computerLower=="scissors")) {
-            computerCounter++;
-            alert(`You lose, ${computerLower} beats ${playerLower}. Current score: you: ${playerCounter}; computer: ${computerCounter}`);
-           
-        }
-        else if ((playerLower=="paper" && computerLower=="rock") || (playerLower=="rock" && computerLower=="scissors") || (playerLower=="scissors" && computerLower=="paper")) {
-           
-            playerCounter++;
-            alert(`You win, ${playerLower} beats ${computerLower}. Current score: you: ${playerCounter}; computer: ${computerCounter}`);
-        }
-        else if (playerLower != "rock" || playerLower!= "paper" || playerLower != "scissors"){
-            alert("Insert a correct decision!");
-            i--;
-        }
+function reset() {
+    playerCounter=0;
+    computerCounter=0;
+}
+
+function game(playerLower) {
+    
+    console.log(playerLower);
+    let computerLower = computerPlay().toLowerCase();
+    if (playerLower == computerLower) {
+        playerCounter++;
+        computerCounter++;
+        alert(`It's a draw! Current score: you: ${playerCounter}; computer: ${computerCounter}`);           
     }
-    if (playerCounter == computerCounter) {alert("You are as smart as a computer!");}
-    else if (playerCounter > computerCounter) {alert("You won the whole game! Congratulations!");}
-    else if (playerCounter < computerCounter) {alert("You lost the whole game :( But you can try again!");}
+    else if ((playerLower=="rock" && computerLower=="paper") || (playerLower=="scissors" && computerLower=="rock") || (playerLower=="paper" && computerLower=="scissors")) {
+        computerCounter++;
+        alert(`You lose, ${computerLower} beats ${playerLower}. Current score: you: ${playerCounter}; computer: ${computerCounter}`);
+    }
+    else if ((playerLower=="paper" && computerLower=="rock") || (playerLower=="rock" && computerLower=="scissors") || (playerLower=="scissors" && computerLower=="paper")) {       
+        playerCounter++;
+        alert(`You win, ${playerLower} beats ${computerLower}. Current score: you: ${playerCounter}; computer: ${computerCounter}`);
+    }
+    if (playerCounter == 5 || computerCounter ==5){
+        if (playerCounter == computerCounter) {
+            alert("You are as smart as a computer! I bet you won't be able to repeat that!");
+            reset();
+        }
+        else if (playerCounter > computerCounter) {
+            alert("You won the whole game! Congratulations! It had to be just a mistake in computer's calculation... I dare you repeat that score!");
+            reset();
+        }
+        else if (playerCounter < computerCounter) {
+            alert("You lost the whole game :( But you can try again!");
+            reset();
+    
+        }
+    };
+    playerScore.innerText = playerCounter;
+    compScore.innerText = computerCounter;
+    
 }
  
+let playerCounter =0;
+let computerCounter =0;
+const div = document.querySelector(".main");
+const img = document.querySelector(".compChoice")
+const playerScore = document.querySelector(".playerScore")
+const compScore = document.querySelector(".compScore")
+
+
+let btns = document.querySelectorAll('button');
  
-game();
+ 
+btns.forEach((button) => {
+  button.addEventListener('click', () => {
+    game(button.id);
+  });
+});
+
